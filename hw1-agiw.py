f2891f00638023195f1aab0b25df8b70f9d49ff3
FedericoYusteenappar <federico.yusteenappar@gmail.com>
2018-04-05 17:54:12 +0200

    modified hw1-agiw.py

--- hw1-agiw.py
+++ hw1-agiw.py
@@ -1,42 +1,11 @@
 import json
-import requests
 import os
-from requests import HTTPError
+from urllib import request
 
 
-def readingfile():
+def reading_file_json():
     data = json.load(open('data.json'))
-    return list(data)
-
-
-# def loadpage():
-#     listelements = readingfile()
-#
-#     for elem in listelements:
-#         req = urllib.request.Request(elem)
-#         with urllib.request.urlopen(req) as response:
-#             the_page = response.read()
-
-
-# def get_headers_from_json():
-#     """
-# T  he function takes a json file with html_body and returns a list of headers.
-# It parses the headers, based on tags starting with 'h'.
-# It also parses urls and return them in a tuple: (headers, url)
-# """
-#     data = json.loads(open('data.json').read())
-#     pattern = "h1" # | h2 | h3"
-#     # pattern = re.compile("^(?!.(href)).^h", re.IGNORECASE)
-#     headers_urls = []
-#     all_headers = []
-#     for x in tqdm(data):
-#         soup = BeautifulSoup(x[0], 'html.parser')
-#         html_headers = soup.find_all(re.compile(pattern))
-#         all_headers.append(html_headers)
-#         url = x[1]
-#         tuple_title_url = all_headers, url
-#         headers_urls.append(tuple_title_url)
-#         return headers_urls
+    return data
 
 
 def create_category_dir():
@@ -50,36 +19,40 @@ create_category_dir()
 
 
 def load_url():
-    global cont
 
-    data = json.load(open('data.json'))
-    for key, value in data.items():
+    data = reading_file_json()
 
-        domain_path = key
+    for key, values in data.items():
+        counter = 0
+        for value in values:
 
-        if not os.path.exists(monitor + "/" + domain_path):
-            os.makedirs(monitor + "/" + domain_path)
+            domain_path = key
 
-        cont = cont + 1
+            if not os.path.exists(monitor + "/" + domain_path):
+                os.makedirs(monitor + "/" + domain_path)
 
-        complete_name_html = os.path.join(monitor + "/" + domain_path, f'{cont}.html')
-        complete_name_index = os.path.join(monitor + "/" + domain_path, "index.txt")
+            counter += 1
 
-        if not os.path.exists(complete_name_index):
-            index = open(complete_name_index, 'w')
+            complete_name_html = os.path.join(monitor + "/" + domain_path, f'{counter}.html')
+            complete_name_index = os.path.join(monitor + "/" + domain_path, "index.txt")
+
+            index = open(complete_name_index, 'a')
 
             try:
-                site = requests.get(value)
-                data = site.text
+
+                site = request.urlopen(value)
+                data = site.read()
 
                 file_html = open(complete_name_html, "wb")  # open file in binary mode
-                index.write(f'{value} \t {cont}.html')
 
-                file_html.write(str.encode(data))
+                index.write(f'{value} \t {counter}.html\n')
+                file_html.write(data)
                 file_html.close()
 
-            except HTTPError:
-                index.write(f'{value} \t {site.status_code}')
+            except (request.HTTPError, request.URLError) as code:
+                index.write(f'{value} \t {code}\n')
+            except ConnectionResetError:
+                index.write(f'{value} \t Connection reset error\n')
 
             index.close()
 
